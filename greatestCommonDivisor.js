e8e033b3ea25783837eadb424f35c687b57dcfee
gyurel <gyurel@yahoo.com>
2022-05-21 19:48:00 +0200

    corrected function

--- greatestCommonDivisor.js
+++ greatestCommonDivisor.js
@@ -3,15 +3,15 @@ function funcionGreatestDevisor(...params){
     let number1 = params[0]
     let number2 = params[1]
     let result = 0
+    let smallest = Math.min(...input)
 
-    for(let i = 1; i < Math.min(input) + 1; i++){
-        if(number1 % i === 0 && number2 % i === 0){
-            if(index > result){
-                result = index
-            }
+    for (let i = 1; i < smallest + 1; i++) {
+        if(number1 % i == 0 && number2 % i == 0) {
+            result = i;
         }
     }
     console.log(result)
 }
 
 funcionGreatestDevisor(15, 5)
+funcionGreatestDevisor(2154, 458)
