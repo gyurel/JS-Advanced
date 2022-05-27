65134199d5aa573e06b98d5b413897b4686467f4
gyurel <gyurel@yahoo.com>
2022-05-27 23:50:56 +0200

    Arrays-Nested arrays problem1

---
+++ printArrayWithDelimiter.js
@@ -0,0 +1,20 @@
+function printerFunc(arr, delimiter){
+    console.log(arr.join(delimiter))
+}
+
+printerFunc(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+)
+
+printerFunc(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+)
