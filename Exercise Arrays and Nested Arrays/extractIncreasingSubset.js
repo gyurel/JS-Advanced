d4eee40b4609f82cc336e20f082dd68ac2cfee93
gyurel <gyurel@yahoo.com>
2022-05-28 19:34:04 +0200

    corrected edgecase

--- Exercise Arrays and Nested Arrays/extractIncreasingSubset.js
+++ Exercise Arrays and Nested Arrays/extractIncreasingSubset.js
@@ -1,7 +1,7 @@
 function extractNonDecreasingSubset(arr){
     let newArray = []
     arr.reduce((acumulator, currentValue)=>{
-        if (currentValue - acumulator > 0){
+        if (currentValue - acumulator >= 0){
             newArray.push(currentValue);
             return acumulator = currentValue
         } else{
@@ -12,10 +12,4 @@ function extractNonDecreasingSubset(arr){
     return newArray
 }
 
-extractNonDecreasingSubset([20, 
-    3, 
-    2, 
-    15,
-    6, 
-    1]    
-    )
+extractNonDecreasingSubset([])
