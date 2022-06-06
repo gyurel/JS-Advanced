ba417515892089481be9acbf089017c14ba02bcb
gyurel <gyurel@yahoo.com>
2022-06-06 16:36:43 +0200

    problem 4 - search in list

--- Exercisе DOM-Introduction/04. Search in List/search.js
+++ Exercisе DOM-Introduction/04. Search in List/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+    let elements = document.querySelectorAll('ul#towns li');
+    let searchText = document.getElementById('searchText').value;
+    let resultTextElement = document.getElementById('result');
+    let matches = 0
+
+    for (const element of elements) {
+        element.style.fontWeight = 'normal';
+        element.style.textDecoration = ''
+    }
+
+    for (let element of elements) {
+        let currentText = element.textContent;
+
+        if (currentText.match(searchText)){
+            matches++;
+            element.style.fontWeight = 'bold';
+            element.style.textDecoration = 'underline'
+        }
+    }
+
+    let resultText = `${matches} matches found`
+
+    resultTextElement.textContent = resultText
 }
