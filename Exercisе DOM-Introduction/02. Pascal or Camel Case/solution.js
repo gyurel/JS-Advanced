083ef1f22737c70559b45631ea7ffdae79a83f0b
gyurel <gyurel@yahoo.com>
2022-06-06 13:48:29 +0200

    problem2 - pascal or camel case

--- Exercisе DOM-Introduction/02. Pascal or Camel Case/solution.js
+++ Exercisе DOM-Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+    let text = document.getElementById('text').value.toLocaleLowerCase().split(' ');
+    let caseToTransform = document.getElementById('naming-convention').value;
+    let result = document.getElementById('result');
+
+    let resultString = ''
+
+    if (caseToTransform == 'Camel Case'){
+        for (let index = 0; index < text.length; index++){
+            if (index > 0) {
+                let currentString = text[index][0].toUpperCase() + text[index].slice(1);
+                resultString = resultString + currentString;
+            } else {
+                resultString = resultString + text[index];
+            }
+        }
+
+    } else if (caseToTransform == 'Pascal Case'){
+        for (let index = 0; index < text.length; index++){
+            let currentString = text[index][0].toUpperCase()  + text[index].slice(1);
+            resultString = resultString + currentString;
+        }
+
+    } else {
+        resultString = 'Error!';
+    }
+
+    result.textContent = resultString;
 }
