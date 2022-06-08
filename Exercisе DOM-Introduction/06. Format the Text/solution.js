fbbb7035406b540f042a30d37acfdfad45e5e62c
gyurel <gyurel@yahoo.com>
2022-06-08 19:59:17 +0200

    problem6 - edit the text

--- Exercisе DOM-Introduction/06. Format the Text/solution.js
+++ Exercisе DOM-Introduction/06. Format the Text/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO
+  let inputTextArr = document.getElementById('input').value.split('.');
+  let outputElement = document.getElementById('output');
+
+  while (length.inputTextArr > 0){
+    let firstSentence = inputTextArr.shift() + '.';
+    let secondSentence = (inputTextArr.shift() == undefined) ? '': inputTextArr.shift() + '.';
+    let thirdSentence = (inputTextArr.shift() == undefined) ? '': inputTextArr.shift() + '.';
+
+    let currentParagraphText = firstSentence + secondSentence + thirdSentence;
+    let p = document.createElement('p')
+    p.textContent = currentParagraphText
+    outputElement.appendChild(p)
+  }
 }
