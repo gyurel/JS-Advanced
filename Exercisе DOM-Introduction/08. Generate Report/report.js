cd8f1b16b1c6d49000a9b6c191046c9248712f09
gyurel <gyurel@yahoo.com>
2022-06-10 22:09:51 +0200

    problem8 - generate report

--- Exercisе DOM-Introduction/08. Generate Report/report.js
+++ Exercisе DOM-Introduction/08. Generate Report/report.js
@@ -1,3 +1,34 @@
 function generateReport() {
-    //TODO
+    let tableHead = Array.from(document.querySelectorAll('thead tr th')) 
+    // tableHead.forEach(x => {
+    //     if(x.checked){
+    //         return x
+    //     }
+    // })
+    let tableRows = document.querySelectorAll('tbody tr');
+    let outputArea = document.getElementById('output');
+    // console.log(tableRows)
+    // console.log(tableHead)
+
+    let resultArr = []
+
+    for (let index = 0; index < tableHead.length; index++){
+        let currentHeadElement = tableHead[index];
+        if (currentHeadElement.firstElementChild.checked === true){
+            for (let rowindex = 0; rowindex < tableRows.length; rowindex++){
+
+                let currentData = tableRows[rowindex].children[index].textContent;
+
+                if(resultArr[rowindex]){
+                    resultArr[rowindex][currentHeadElement.textContent.toLowerCase().trim()] = currentData;
+                } else {
+                    resultArr[rowindex] = {};
+                    resultArr[rowindex][currentHeadElement.textContent.toLowerCase().trim()] = currentData;
+                }
+            }
+        }
+    }
+
+    outputArea.textContent = JSON.stringify(resultArr)
+
 }
