10930679edbbd4e404e06abf867b357954f8f077
gyurel <gyurel@yahoo.com>
2022-06-11 12:25:27 +0200

    roblem1 - time converter

--- Exercise DOM Manipulations and Events/01. Sections/script.js
+++ Exercise DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   let contentDiv = document.getElementById('content')
+
+   for (let word of words){
+      let divElement = document.createElement('div');
+      let paragraph = document.createElement('p');
+      paragraph.textContent = word;
+      paragraph.style.display = 'none';
+      divElement.addEventListener('click', onClick);
+      divElement.appendChild(paragraph);
+      contentDiv.appendChild(divElement);
+   }
+
+   function onClick(event){
+      event.currentTarget.children[0].style.display = '';
+   }
 }
