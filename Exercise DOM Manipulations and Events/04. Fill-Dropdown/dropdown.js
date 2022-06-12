6e31baecf3c7aec9cad4de2420dd63c32f4adcd8
gyurel <gyurel@yahoo.com>
2022-06-12 14:27:44 +0200

    problem4 - fill dropdown

--- Exercise DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ Exercise DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+    let selectItem = document.getElementById('menu');
+
+    let newOptionElement = document.createElement('option');
+    newOptionElement.textContent = newItemText.value;
+    newOptionElement.value = newItemValue.value;
+    selectItem.appendChild(newOptionElement);
+
+    newItemText.value = '';
+    newItemValue.value = '';
 }
