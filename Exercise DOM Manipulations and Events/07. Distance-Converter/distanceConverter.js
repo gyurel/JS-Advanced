2399f1035192da8bde2a6cffa423c17c6ffccf2d
gyurel <gyurel@yahoo.com>
2022-06-12 23:23:10 +0200

    problem 7 - distance converter

--- Exercise DOM Manipulations and Events/07. Distance-Converter/distanceConverter.js
+++ Exercise DOM Manipulations and Events/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,20 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    document.getElementById('convert').addEventListener('click', convert);
+    let inputField = document.getElementById('inputDistance');
+    let outputField = document.getElementById('outputDistance');
+    let inputUnitsMenu = document.getElementById('inputUnits');
+    let outputUnitsMenu = document.getElementById('outputUnits');
+
+    let ratiosToMeter = [1000, 1, 0.01, 0.001, 1609.34, 0.9144, 0.3048, 0.0254]
+
+
+    function convert(e){
+        let inputValue = inputField.value;
+        // console.log(inputValue)
+        let inputUnit = inputUnitsMenu.selectedIndex;
+        let outputUnit = outputUnitsMenu.selectedIndex;
+
+        let outputValue = (inputValue * ratiosToMeter[inputUnit]) / ratiosToMeter[outputUnit];
+        outputField.value = outputValue;
+    }
 }
