2f094ff8b4d1b14a9fa46f206cacd8a7c84e827e
gyurel <gyurel@yahoo.com>
2022-06-12 02:30:53 +0200

    problem3 - locked profile

--- Exercise DOM Manipulations and Events/03. Locked-Profile/app.js
+++ Exercise DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+    
+    Array.from(document.querySelectorAll('.profile button')).forEach(b => b.addEventListener('click', lockUnlockFunk));
+
+    function lockUnlockFunk(e){
+        let currentRadioBtnStatus = e.target.parentElement.querySelectorAll('.profile input[value="lock"]')[0];
+        let currentUserFields = e.target.parentElement.getElementsByTagName('div')[0];
+        let button = e.target
+
+        if (!currentRadioBtnStatus.checked){
+            if ( button.textContent === 'Show more'){
+                button.textContent = 'Hide it';
+                currentUserFields.style.display = 'block';
+            } else {
+                button.textContent = 'Show more';
+                currentUserFields.style.display = '';
+            }   
+        }
+    }
 }
