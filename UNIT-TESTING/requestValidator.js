5fa89a7989ad172d5ff962fc96386162c38dc8ab
gyurel <gyurel@yahoo.com>
2022-06-17 01:24:31 +0200

    problem 1 - request validator

---
+++ UNIT-TESTING/requestValidator.js
@@ -0,0 +1,33 @@
+function validator(request) {
+    let methods = ['GET', 'POST', 'DELETE', 'CONNECT'];
+    let versions = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
+
+    uriRegex = /^[\w.]+$/;
+    specialCharactersRegex = /^[^<>\\&\'\"]+$/;
+
+    if (!(request.method && methods.includes(request.method))) {
+        throw new Error("Invalid request header: Invalid Method")
+    };
+
+    if (!(request.uri && (request.uri == '*' || uriRegex.test(request.uri)))) {
+        throw new Error("Invalid request header: Invalid URI")
+    };
+
+    if (!(request.version && versions.includes(request.version))) {
+        throw new Error("Invalid request header: Invalid Version")
+    };
+
+    if (!(request.hasOwnProperty('message') && (request.message == '' || specialCharactersRegex.test(request.message)))){
+        throw new Error("Invalid request header: Invalid Message")
+    };
+
+    return request
+}
+
+validator({
+    method: 'GET',
+    uri: 'svn.public.catalog',
+    version: 'HTTP/1.1',
+    message: ''
+}
+)
