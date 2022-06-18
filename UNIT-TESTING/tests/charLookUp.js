75f09ea63bb25e050cf083c4be9e475217a27fcb
gyurel <gyurel@yahoo.com>
2022-06-18 18:41:24 +0200

    problem3 - character lookup

---
+++ UNIT-TESTING/tests/charLookUp.js
@@ -0,0 +1,35 @@
+let expect = require('chai').expect;
+
+function lookupChar(string, index) {
+    if (typeof(string) !== 'string' || !Number.isInteger(index)) {
+        return undefined;
+    }
+    if (string.length <= index || index < 0) {
+        return "Incorrect index";
+    }
+
+    return string.charAt(index);
+}
+
+describe('lookupChar Tests', () => {
+    it('returning undefined when first argument different from string', () => {
+        expect(lookupChar(8, 5)).to.be.undefined;
+    })
+
+    it('return undefined if index different than interger', () => {
+        expect(lookupChar('string', 3.5)).to.be.undefined;
+    })
+
+    it('return "incorrect index" when index smaller than zero', () => {
+        expect(lookupChar('string', -1)).to.equal('Incorrect index');
+    })
+
+    it('return "incorrect index" when index bigger than length minus one', () => {
+        currentString = 'string'
+        expect(lookupChar(currentString, currentString.length)).to.equal('Incorrect index');
+    })
+
+    it('return the char at the index if everything is correct', () => {
+        expect(lookupChar('string', 0)).to.equal('s');
+    })
+})
