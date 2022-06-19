293cba3cac72802e7e901138fe4e5ed7d13d8347
gyurel <gyurel@yahoo.com>
2022-06-19 18:29:01 +0200

    problem6 - dynamic validation

--- UNIT-TESTING/06.Dynamic-Validation/app.js
+++ UNIT-TESTING/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+    let emailInput = document.getElementById('email');
+    let regex = new RegExp('^[a-z]+@[a-z]+\.[a-z]+');
+
+    emailInput.addEventListener('change', () => {
+        if(regex.test(emailInput.value)){
+            emailInput.classList.remove('error')
+        } else {
+            emailInput.classList.add('error')
+        }
+    })
 }
